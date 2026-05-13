05cae57e8458dc3926d545d526e72c18172d59ad
nkgarg21b <nkgarg21@gmail.com>
2026-05-13 19:51:25 +0530

    Add activation hooks and bootstrap systems

--- wp-investor-panel.php
+++ wp-investor-panel.php
@@ -20,9 +20,25 @@ define( 'WIP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WIP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 require_once WIP_PLUGIN_PATH . 'includes/core/class-wip-loader.php';
+require_once WIP_PLUGIN_PATH . 'includes/core/class-wip-roles.php';
+require_once WIP_PLUGIN_PATH . 'includes/database/class-wip-db-installer.php';
+
+/**
+ * Plugin activation hook.
+ *
+ * @return void
+ */
+function wip_activate_plugin() {
+    WIP_Roles::register_roles();
+    WIP_DB_Installer::install();
+}
+
+register_activation_hook( __FILE__, 'wip_activate_plugin' );
 
 /**
  * Initialize plugin.
+ *
+ * @return void
  */
 function wip_initialize_plugin() {
     $loader = new WIP_Loader();
