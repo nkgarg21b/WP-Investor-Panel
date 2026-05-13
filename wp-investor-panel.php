bd6224b5ef6380831159db0a8a4f9708f8e60c17
nkgarg21b <nkgarg21@gmail.com>
2026-05-13 19:48:32 +0530

    Add plugin bootstrap file

---
+++ wp-investor-panel.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: WP Investor Panel
+ * Plugin URI: https://github.com/nkgarg21b/WP-Investor-Panel
+ * Description: Enterprise-grade investor management and farm transparency platform.
+ * Version: 1.0.0
+ * Author: Nikhil Garg
+ * Text Domain: wp-investor-panel
+ * Domain Path: /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Plugin constants.
+define( 'WIP_VERSION', '1.0.0' );
+define( 'WIP_PLUGIN_FILE', __FILE__ );
+define( 'WIP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'WIP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+require_once WIP_PLUGIN_PATH . 'includes/core/class-wip-loader.php';
+
+/**
+ * Initialize plugin.
+ */
+function wip_initialize_plugin() {
+    $loader = new WIP_Loader();
+    $loader->init();
+}
+
+add_action( 'plugins_loaded', 'wip_initialize_plugin' );
